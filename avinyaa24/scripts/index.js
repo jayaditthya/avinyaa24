7923549891af6dc23e8a1030989f109ad5587298
Jayaram Aditthya <113106842+jayaditthya@users.noreply.github.com>
2024-01-24 09:51:06 +0530

    Add files via upload

---
+++ avinyaa24/scripts/index.js
@@ -0,0 +1,77 @@
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("Navbar").style.top = "0";
+//   } else {
+//     document.getElementById("Navbar").style.top = "-150px";
+//   }
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+    console.log(obj, 1);
+  }
+  counter("count1", 0, 3, 1000);
+  counter("count2", 0, 4, 1000);
+});
+
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy,
+    dayMonth = "02/05/",
+    birthday = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+    }, 0);
+})();
+
+function showReadMore(element) {
+  element.querySelector(".iconsonhover").style.display = "flex";
+}
+
+function hideReadMore(element) {
+  element.querySelector(".iconsonhover").style.display = "none";
+}
